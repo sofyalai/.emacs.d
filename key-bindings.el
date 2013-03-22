c150d7fcc0f8f42d3bc13a1d0d16c5ab82a43ca3
Fredrik Nyqvist <fredrik.nyqvist94@gmail.com>
2013-03-22 23:14:58 +0100

    updated to work wit evil-mode

--- key-bindings.el
+++ key-bindings.el
@@ -1,14 +1,14 @@
 ;; multiple cursors
 (global-set-key (kbd "M-l") 'mc/mark-next-like-this)
 (global-set-key (kbd "M-L") 'mc/mark-previous-like-this)
-;; (global-set-key (kbd "C-M-z") 'mc/edit-lines)
-;; (global-set-key (kbd "C->") 'mc/mark-next-like-this)
-;; (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-;; (global-set-key (kbd "M-n") 'mc/insert-numbers)
-;; (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-M-z") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "M-n") 'mc/insert-numbers)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
-;; (define-key evil-normal-state-map "b" 'ido-switch-buffer)
-;; (define-key evil-normal-state-map "E" 'ido-find-file)
+(define-key evil-normal-state-map "b" 'ido-switch-buffer)
+(define-key evil-normal-state-map "E" 'ido-find-file)
 
 ;; org-mode
 (global-set-key "\C-cl" 'org-store-link)
@@ -30,7 +30,7 @@
 (global-set-key "\C-c\C-d" "\C-a\C- \C-n\M-w\C-y\C-p")
 
 ;; ace-jump
-;(define-key evil-normal-state-map "  " 'ace-jump-mode)
+(define-key evil-normal-state-map "  " 'ace-jump-mode)
 
 ;;----Delete/Backspace settings----
 ;; Delete deleted char right of cursor
@@ -47,19 +47,11 @@
 (define-key isearch-mode-map [backspace] 'isearch-delete-char)
 
 (global-set-key (kbd "C-c C-r") 'revert-buffer)
-(global-set-key (kbd "C-x c o") 'clearcase-checkout-current-buffer)
-(global-set-key (kbd "C-x v c") 'clearcase-uncheckout-current-buffer)
-(global-set-key (kbd "C-x c u") 'clearcase-checkout-current-buffer-unreserved)
-(global-set-key (kbd "C-x c i") 'clearcase-checkin-current-buffer)
 (global-set-key (kbd "<f1>") 'linum-mode)
-(global-set-key (kbd "<f2>") 'theme-solarized)
-(global-set-key (kbd "<f3>") 'theme-zenburn)
+(global-set-key (kbd "<f2>") 'theme-blue)
+(global-set-key (kbd "<f3>") 'theme-dark)
 (global-set-key (kbd "<f4>") 'goto-line)
 (global-set-key (kbd "<f5>") 'revert-buffer-no-confirm)
-(global-set-key (kbd "<f6>") 'vhdl-align-group)
-(global-set-key (kbd "<f7>") 'sr-speedbar-toggle)
-(global-set-key (kbd "<f8>") 'output-file-highlight)
-(global-set-key (kbd "<f9>") 'output-file-unhighlight)
 (global-set-key (kbd "<M-down>") 'move-line-down)
 (global-set-key (kbd "<M-up>") 'move-line-up)
 (global-set-key (kbd "C-'") 'copy-line)
@@ -70,12 +62,12 @@
 (global-set-key [pause] 'toggle-current-window-dedication)
 
 ;; esc quits
-;; (define-key evil-normal-state-map [escape] 'keyboard-quit)
-;; (define-key evil-visual-state-map [escape] 'keyboard-quit)
-;; (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-;; (define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-;; (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-;; (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-;; (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 
 (provide 'key-bindings)
