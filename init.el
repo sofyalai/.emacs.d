f4a45d58c58b37b1831d03741afb3906d80cd0dd
Fredrik Nyqvist <fredrik.nyqvist94@gmail.com>
2013-04-21 23:15:39 +0200

    fixed files

--- init.el
+++ init.el
@@ -1,20 +1,16 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/elisp"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/elisp/ac-dict"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/elisp/eshell"))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/elisp/multiple-cursors"))
 
 (require 'iso-transl)
 (require 'init-auctex)
 (require 'init-auto-complete)
 (require 'init-csharp)
 (require 'init-misc)
+(require 'init-multiple-cursors)
 (require 'init-org)
 (require 'init-themes)
 (require 'init-vala)
 (require 'init-evil)
-
-(require 'multiple-cursors)
 
 (require 'emacs-settings)
 (require 'key-bindings)
